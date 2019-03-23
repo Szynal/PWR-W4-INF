436cce105168aec39cebf905dc0c1b17b2e8facc
Paweł Szynal <pawel.szynal@email.com>
2019-03-23 19:58:17 +0100

    [PWR Java-lab-04] Create NoteLabel.java

---
+++ Programowanie w języku Java/lab04/Timetable/src/core/NoteLabel.java
@@ -0,0 +1,26 @@
+package core;
+
+import java.util.Arrays;
+
+public enum NoteLabel {
+	NOT_IMPORTANT("Not important"), IMPORTANT("IMPORTANT"), CONTACT("Contact"), LONG_TERM("Long term"),;
+
+	private String value;
+
+	NoteLabel(final String value) {
+		this.value = value;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public static NoteLabel fromText(String text) {
+		return Arrays.stream(values()).filter(bl -> bl.value.equalsIgnoreCase(text)).findFirst().orElse(null);
+	}
+
+	@Override
+	public String toString() {
+		return this.getValue();
+	}
+}
